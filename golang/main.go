de0eda20dd1709370d8ab2d100e7862f615acb3e
yokogawa-k <yokogawa-k@klab.com>
2020-04-06 13:54:59 +0900

    Fix sample code

--- golang/main.go
+++ golang/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-  "github.com/k0kubun/pp"
+	"github.com/k0kubun/pp"
 )
 
 func main() {
-  m := map[string]string{"foo": "bar", "hello": "world"}
+	m := map[string]string{"foo": "bar", "hello": "world"}
   pp.Print(m)
 }
